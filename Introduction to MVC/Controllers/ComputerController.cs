d2fe2aa069ca848127646501f41ec869e66f87a3
Dalia Sayaheen <dalia.mahmoudsayaheen@gmail.com>
2021-01-31 21:56:30 +0200

    lesson 3 Create Model And Controller And View

---
+++ Introduction to MVC/Controllers/ComputerController.cs
@@ -0,0 +1,24 @@
+﻿using Introduction_to_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Introduction_to_MVC.Controllers
+{
+    public class ComputerController : Controller
+    {
+        // GET: Computer
+        public ActionResult Display()
+        {
+            var MyComputer = new ComputerModel()
+            {
+                Name = "lenovo",
+                Price = 500 
+            };
+            return View(MyComputer);
+           
+        }
+    }
+}
---
+++ Introduction to MVC/Models/ComputerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Introduction_to_MVC.Models
+{
+    public class ComputerModel
+    {
+        
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Double Price { get; set; }
+
+        
+    }
+}
