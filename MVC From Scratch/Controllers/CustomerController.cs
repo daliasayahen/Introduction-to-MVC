4b72f22cbfa96daee0bf8ee37da00700876c3cd1
Dalia Sayaheen <dalia.mahmoudsayaheen@gmail.com>
2021-02-02 20:17:18 +0200

    lesson 8 Working with Model using Razor

---
+++ MVC From Scratch/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_From_Scratch.Models;
+
+namespace MVC_From_Scratch.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult DisplayCostumer()
+        {
+            var CustomerModel = new CustomerModel(1 ,"dalia" , "AMMAN",26);
+            return View(CustomerModel);
+        }
+    }
+}
---
+++ MVC From Scratch/Models/CustomerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_From_Scratch.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address{ get; set; }
+        public int Age { get; set; }
+
+        public CustomerModel(int id, string name , string adress , int age)
+        {
+            Id = id;
+            Name = name;
+            Address = adress;
+            Age = age;
+
+
+        }
+    }
+}
